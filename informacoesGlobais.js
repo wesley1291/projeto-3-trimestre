facbd78224834867ddc6ffa7fdc5b404a32586e7
Wesley <130110338+wesley1291@users.noreply.github.com>
2024-10-08 12:18:50 -0300

    Add files via upload

--- informacoesGlobais.js
+++ informacoesGlobais.js
@@ -4,12 +4,21 @@ console.log(url);
 async function visualizarInformacoesGlobais (){
    const res = await fetch(url)
    const dados = await res.json()
+   const pessoasConectadas = (dados.total_pessoas_mundo)/1e9
+   const pessoasMundo = (dados.total_medio)
+   const horas = parseInt(dados.medio)
+   const minutos = Math.round ((dados.tempo_medio- horas)*100)
+   const porcentagemConectadas = ((pessoasConectadas / pessoasMundo)*100).toFixed(2)
    const paragrafo = document.createElement('p')
    paragrafo.classList.add('graficos-container_texto')
    paragrafo.innerHTML = `Você sabia que o mundo tem
-   ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas}
-   estao conectadas em algum rede social e passam em média ${dados.tempo_médio} horas conectadas.`
-   const container = document.getElementById('graficos-caontaiter')
+    <span>${pessoasMundo}</span> de pessoas e 
+    que aproximadamente <span>${pessoasConectadas}</span>
+     estao conectadas em alguma rede social e passam em media
+     <span>${horas}</span> horas e <span>${minutos}</span> minutos conectados.
+     <br> Isso significa que aproximadamente ${porcentagemConectadas}%
+     de pessoas estao conectadas em alguma rede social.`
+   const container = document.getElementById('graficos-contaiter')
    container.appendChild(paragrafo)
    console.log(dados.tempo_médio)
    
@@ -20,4 +29,3 @@ visualizarInformacoesGlobais()
     "total_medio"; 2.38,
     "total_pessoas_mundo"; 7.888e9
 }
-paragrafo.innerHTML = `você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estao conectadas em alguma rede social e passam em media <span>${dados.tempo_médio}</span> horas conectadas.`
